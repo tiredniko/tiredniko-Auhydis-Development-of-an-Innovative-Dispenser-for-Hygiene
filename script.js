5ebca6203a3a2078684cb7b5f6ac8f5f2c47160d
tiredniko <nikopaned.official@gmail.com>
2024-07-25 22:08:24 +0800

    script.js

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function displayCart() {
     let total = 0;
     cart.forEach(item => {
         const li = document.createElement('li');
-        li.textContent = `${item.product} - $${item.price.toFixed(2)}`;
+        li.textContent = `${item.product} - ₱${item.price.toFixed(2)}`;
         cartItems.appendChild(li);
         total += item.price;
     });
-    totalPrice.textContent = `Total: $${total.toFixed(2)}`;
+    totalPrice.textContent = `Total: ₱${total.toFixed(2)}`;
 }
 
 function checkout() {
