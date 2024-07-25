5706d22d500c343cfaa1bedafb2d2b9bbc40a4d7
tiredniko <nikopaned.official@gmail.com>
2024-07-25 21:52:55 +0800

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('Welcome to my website!');
+function purchase(product) {
+    alert('You have selected ' + product);
+    // Implement the payment and communication with the vending machine here.
+    // This will likely involve AJAX calls to your backend server to handle payments and dispense the product.
+}
